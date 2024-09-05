1060559381376bbf57c7250103eee2be0fdde8e0
unknown <razag5537@gmail.com>
2024-09-05 17:47:48 +0530

    solving holidays issues

--- src/views/Holidays.tsx
+++ src/views/Holidays.tsx
@@ -19,10 +19,12 @@ import AddIcon from '@mui/icons-material/Add';
 import { DriveFileRenameOutlineOutlined } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
+import { format } from 'date-fns'
 
 import type { AppDispatch, RootState } from '@/redux/store';
 import { fetchHolidays } from '@/redux/features/holidays/holidaysSlice';
 import 'react-toastify/dist/ReactToastify.css';
+import AddHolidayForm from '@/components/holiday/HolidayForm';
 
 export default function HolidayGrid() {
   const dispatch: AppDispatch = useDispatch();
@@ -34,6 +36,7 @@ export default function HolidayGrid() {
   const [selectedKeyword, setSelectedKeyword] = useState('');
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+  const [isHalfDay, setIsHalfDay] = useState(false);
 
   const debouncedFetch = useCallback(
     debounce(() => {
@@ -69,248 +72,6 @@ export default function HolidayGrid() {
     setUserId(user.id);
   }, []);
 
-  function AddHolidayForm({ handleClose, holiday }) {
-    const [formData, setFormData] = useState({
-      title: '',
-      note: '',
-      year: '',
-      start_date: '',
-      end_date: '',
-    });
-
-    const [errors, setErrors] = useState({
-      title: '',
-      note: '',
-      year: '',
-      start_date: '',
-      end_date: '',
-    });
-
-    useEffect(() => {
-      if (holiday) {
-        const selected = holidays.find(h => h._id === holiday);
-
-        if (selected) {
-          setFormData({
-            title: selected.title,
-            note: selected.note,
-            year: selected.year,
-            start_date: selected.start_date,
-            end_date: selected.end_date,
-          });
-        }
-      }
-    }, [holiday, holidays]);
-
-    const validateForm = () => {
-      let isValid = true;
-
-      const newErrors = {
-        title: '',
-        note: '',
-        year: '',
-        start_date: '',
-        end_date: '',
-      };
-
-      if (!formData.title.trim()) {
-        newErrors.title = 'Title is required';
-        isValid = false;
-      }
-
-      if (!formData.start_date) {
-        newErrors.start_date = 'Start date is required';
-        isValid = false;
-      }
-
-      if (!formData.end_date) {
-        newErrors.end_date = 'End date is required';
-        isValid = false;
-      } else if (formData.end_date < formData.start_date) {
-        newErrors.end_date = 'End date must be after start date';
-        isValid = false;
-      }
-
-      if (!formData.note.trim()) {
-        newErrors.note = 'Note is required';
-        isValid = false;
-      }
-
-      if (!formData.year) {
-        newErrors.year = 'Year is required';
-        isValid = false;
-      } else if (isNaN(formData.year) || formData.year.length !== 4) {
-        newErrors.year = 'Year must be a valid 4-digit number';
-        isValid = false;
-      }
-
-      setErrors(newErrors);
-
-      return isValid;
-    };
-
-
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-
-      setFormData(prevState => ({
-        ...prevState,
-        [name]: value,
-      }));
-    };
-
-    const handleSubmit = () => {
-      if (validateForm()) {
-        const method = holiday ? 'PUT' : 'POST';
-        const url = holiday ? `${process.env.NEXT_PUBLIC_APP_URL}/holidays/update/${holiday}` : `${process.env.NEXT_PUBLIC_APP_URL}/holidays/create`;
-
-        fetch(url, {
-          method,
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(formData),
-        })
-          .then(response => response.json())
-          .then(data => {
-            if (data.message) {
-              if (data.message.includes('success')) {
-                toast.success(data.message, {
-                  position: 'top-center',
-                });
-              } else {
-                toast.error('Error: ' + data.message, {
-                  position: 'top-center',
-                });
-              }
-            } else {
-              toast.error('Unexpected error occurred', {
-                position: 'top-center',
-              });
-            }
-
-            handleClose();
-            debouncedFetch();
-          })
-          .catch(error => {
-            toast.error('Error: ' + error.message, {
-              position: 'top-center',
-            });
-          });
-      }
-    };
-
-
-    return (
-      <Box sx={{ flexGrow: 1, padding: 2 }}>
-        <Box display='flex' justifyContent='space-between' alignItems='center'>
-          <Typography style={{ fontSize: '2em' }} variant='h5' gutterBottom>
-            {holiday ? 'Edit Holiday' : 'Add Holiday'}
-          </Typography>
-          <IconButton onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label='Title'
-              name='title'
-              value={formData.title}
-              onChange={handleChange}
-              required
-              error={!!errors.title}
-              helperText={errors.title}
-              FormHelperTextProps={{
-                style: { color: 'red' }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label='Start Date'
-              name='start_date'
-              value={formData.start_date}
-              type='date'
-              onChange={handleChange}
-              InputLabelProps={{ shrink: true }}
-              required
-              error={!!errors.start_date}
-              helperText={errors.start_date}
-              FormHelperTextProps={{
-                style: { color: 'red' }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label='End Date'
-              name='end_date'
-              type='date'
-              value={formData.end_date}
-              onChange={handleChange}
-              InputLabelProps={{ shrink: true }}
-              required
-              error={!!errors.end_date}
-              helperText={errors.end_date}
-              FormHelperTextProps={{
-                style: { color: 'red' }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label='Note'
-              name='note'
-              value={formData.note}
-              onChange={handleChange}
-              required
-              error={!!errors.note}
-              helperText={errors.note}
-              FormHelperTextProps={{
-                style: { color: 'red' }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              label='Year'
-              name='year'
-              value={formData.year}
-              onChange={handleChange}
-              required
-              error={!!errors.year}
-              helperText={errors.year}
-              FormHelperTextProps={{
-                style: { color: 'red' }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              style={{
-                fontSize: '18px',
-                fontWeight: 600,
-                color: 'white',
-                padding: 15,
-                backgroundColor: '#ff902f',
-                width: 200,
-              }}
-              variant='contained'
-              fullWidth
-              onClick={handleSubmit}
-            >
-              {holiday ? 'UPDATE HOLIDAY' : 'ADD HOLIDAY'}
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    );
-  }
-
   const handleHolidayAddClick = () => {
     setSelectedHoliday(null);
     setShowForm(true);
@@ -326,20 +87,26 @@ export default function HolidayGrid() {
   };
 
   const columns: GridColDef[] = [
+    { field: 'day', headerName: 'Day', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center', flex: 1 },
+    { field: 'title', headerName: 'Title', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center', flex: 1 },
     {
-      sortable: true,
-      field: 'lineNo',
-      headerName: 'S.NO',
+      field: 'start_date',
+      headerName: 'Closing Date',
       headerClassName: 'super-app-theme--header',
-      flex: 0,
-      editable: false,
-      renderCell: (params) => params.api.getAllRowIds().indexOf(params.id) + 1,
+      headerAlign: 'center', align: 'center',
+      flex: 1,
+      renderCell: (params) => format(new Date(params.value), 'dd-MMM-yyyy').toUpperCase(),
     },
-    { field: 'title', headerName: 'Title', headerClassName: 'super-app-theme--header', width: 200, headerAlign: 'center', align: 'center', sortable: false },
-    { field: 'start_date', headerName: 'Holiday Start Date', headerClassName: 'super-app-theme--header', width: 200, headerAlign: 'center', align: 'center', sortable: false },
-    { field: 'end_date', headerName: 'Holiday End Date', headerClassName: 'super-app-theme--header', width: 200, headerAlign: 'center', align: 'center', sortable: false },
-    { field: 'note', headerName: 'Note', headerClassName: 'super-app-theme--header', width: 200, headerAlign: 'center', align: 'center', sortable: false },
-    { field: 'year', headerName: 'Year', headerClassName: 'super-app-theme--header', width: 200, headerAlign: 'center', align: 'center', sortable: false },
+    {
+      field: 'end_date',
+      headerName: 'Opening Date',
+      headerClassName: 'super-app-theme--header',
+      headerAlign: 'center',
+      align: 'center',
+      flex: 1,
+      renderCell: (params) => format(new Date(params.value), 'dd-MMM-yyyy').toUpperCase(),
+    },
+    { field: 'note', headerName: 'Note', headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center', flex: 1 },
     ...(userRole === '1'
       ? [
         {
@@ -367,7 +134,8 @@ export default function HolidayGrid() {
       <Box sx={{ flexGrow: 1, padding: 2 }}>
         <Dialog open={showForm} onClose={handleClose} fullWidth maxWidth="md">
           <DialogContent>
-            <AddHolidayForm holiday={selectedHoliday} handleClose={handleClose} />
+            {/* <AddHolidayForm holiday={selectedHoliday} handleClose={handleClose} /> */}
+            <AddHolidayForm holiday={selectedHoliday} handleClose={handleClose} holidays={holidays} debouncedFetch={debouncedFetch} />
           </DialogContent>
         </Dialog>
         <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
